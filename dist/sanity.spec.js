1004fadf47ea4be3ae2563faa435b42f1ceec248
Daniel Sato <daniel.sato@unizin.org>
2018-08-08 14:04:54 -0500

    Delete compiled assets

--- dist/sanity.spec.js
+++
@@ -1,7 +0,0 @@
-import './esm/index.js';
-import './cjs/index.js';
-
-describe('final sanity test', () => {
-    it('should load compiled esm without crashing', () => {});
-    it('should load compiled commonJS without crashing', () => {});
-});
