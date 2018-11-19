0cb3e53c82292758289eb12bbc6598fc824259b6
Daniel Sato <dksato@gmail.com>
2018-11-19 17:29:41 -0600

    Add "first" and "last" buttons to pagination

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,7 +2,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import {
+    faAngleLeft,
+    faAngleDoubleLeft,
+    faAngleRight,
+    faAngleDoubleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Aural from './aural';
 import theme from '../theme';
@@ -18,15 +23,30 @@ class Pagination extends React.Component<Props> {
     handleClickNext = () => {
         this.props.changePage(this.props.page + 1);
     };
+
     handleClickPrevious = () => {
         this.props.changePage(this.props.page - 1);
     };
+
+    handleClickFirst = () => {
+        this.props.changePage(1);
+    };
+
+    handleClickLast = () => {
+        const { changePage, pageCount } = this.props;
+        changePage(pageCount);
+    };
+
     render = () => {
         const { page, hasNext, pageCount } = this.props;
         if (page === 1 && !hasNext) return null;
         return (
             <PaginationWrapper>
                 <div>
+                    <Button onClick={this.handleClickFirst} disabled={page === 1}>
+                        <FontAwesomeIcon aria-hidden icon={faAngleDoubleLeft} />
+                        <Aural>First page</Aural>
+                    </Button>
                     <Button onClick={this.handleClickPrevious} disabled={page === 1}>
                         <FontAwesomeIcon aria-hidden icon={faAngleLeft} />
                         <Aural>Previous page</Aural>
@@ -39,6 +59,10 @@ class Pagination extends React.Component<Props> {
                     <Button onClick={this.handleClickNext} disabled={!hasNext}>
                         <FontAwesomeIcon aria-hidden icon={faAngleRight} />
                         <Aural>Next page</Aural>
+                    </Button>
+                    <Button onClick={this.handleClickLast} disabled={page === pageCount}>
+                        <FontAwesomeIcon aria-hidden icon={faAngleDoubleRight} />
+                        <Aural>Last page</Aural>
                     </Button>
                 </div>
             </PaginationWrapper>
@@ -63,9 +87,11 @@ const PaginationWrapper = styled.div`
         text-align: center;
     }
 
-    > :not(:nth-child(2)) {
-        button {
-            font-size: 21px;
+    button {
+        font-size: 21px;
+
+        &:first-child {
+            margin-right: ${theme.spacing.tiny};
         }
     }
 `;
