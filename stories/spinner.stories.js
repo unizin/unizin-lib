28fe2754bdfc58f9399f2d8c21aaec25c3344060
Daniel Sato <daniel.sato@unizin.org>
2019-01-30 12:03:52 -0600

    Make spinner better
    Eliminate many uses of CSS calc, in favor of a variable width stroke.

--- stories/spinner.stories.js
+++ stories/spinner.stories.js
@@ -3,4 +3,6 @@ import React from 'react';
 import Spinner from '../src/components/spinner';
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Spinner', module).add('Defaults', () => <Spinner />);
+storiesOf('Spinner', module)
+    .add('Defaults', () => <Spinner />)
+    .add('Order tool version', () => <Spinner width="56px" stroke="10px" />);
