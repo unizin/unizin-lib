f0c84dbe391afc69d79a4fe64c629d24bc9119f0
Daniel Sato <daniel.sato@unizin.org>
2019-12-04 11:35:17 -0600

    Use aria role instead of hidden input

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -5,56 +5,73 @@ import theme from '../theme';
 
 type Props = {
     checked?: boolean,
+    color?: string,
     disabled?: boolean,
-    onChange?: (SyntheticEvent<HTMLInputElement>) => void,
+    height?: string | number,
+    id?: string,
+    onChange?: boolean => void,
 };
 
-export default function Toggle({ checked, disabled, onChange }: Props) {
+export default function Toggle({
+    checked = false,
+    color = theme.colors.green.medium,
+    disabled = false,
+    height = theme.spacing.small,
+    id = null,
+    onChange,
+    ...rest
+}: Props) {
     const [isChecked, setChecked] = useState(checked || false);
-    const onClick = e => {
+    const onClick = () => {
         if (disabled) return;
         setChecked(!isChecked);
-        onChange && onChange(e);
+        onChange && onChange(!isChecked);
+    };
+    const onKeyDown = ({ key }: SyntheticKeyboardEvent<HTMLDivElement>) => {
+        if (key === 'Enter' || key === ' ') {
+            onClick();
+        }
     };
     return (
-        <Wrapper checked={isChecked} onClick={onClick} disabled={disabled}>
-            <input type="checkbox" checked={isChecked} disabled={disabled} onChange={onClick} />
-            <div aria-hidden />
-        </Wrapper>
+        <Wrapper
+            aria-checked={isChecked}
+            checked={isChecked}
+            color={color}
+            disabled={disabled}
+            height={height}
+            id={id}
+            onClick={onClick}
+            onKeyDown={onKeyDown}
+            role="checkbox"
+            {...(disabled ? null : { tabIndex: 0 })}
+            {...rest}
+        />
     );
 }
 
 const Wrapper = styled.div`
-    border: ${theme.borders.default};
-    background: ${({ checked, disabled }) =>
-        disabled
-            ? theme.colors.grey.ultraLight
-            : checked
-            ? theme.colors.green.medium
-            : theme.colors.grey.light};
-    width: ${theme.spacing.medium};
-    height: ${theme.spacing.small};
-    border-radius: ${theme.spacing.small};
-    position: relative;
-
-    > input {
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: 0;
-        opacity: 0;
-    }
-
-    > div {
-        transition: transform ${theme.animationDuration};
-        background: white;
-        border-radius: ${theme.spacing.small};
+    ${({ checked, color, disabled, height }) => `
         border: ${theme.borders.default};
-        height: ${theme.spacing.small};
-        width: ${theme.spacing.small};
-        position: absolute;
-        top: -1px;
-        right: -1px;
-        transform: translateX(${({ checked }) => (checked ? 0 : 'calc(-100% + 2px)')});
-    }
+        background: ${
+            disabled ? theme.colors.grey.ultraLight : checked ? color : theme.colors.grey.light
+        };
+        width: calc(2 * ${height});
+        height: ${height};
+        border-radius: ${height};
+        position: relative;
+
+        &:after {
+            transition: transform ${theme.animationDuration};
+            background: white;
+            border-radius: ${height};
+            border: ${theme.borders.default};
+            content: '';
+            height: ${height};
+            width: ${height};
+            position: absolute;
+            top: -1px;
+            right: -1px;
+            transform: translateX(${checked ? 0 : 'calc(-100% + 2px)'});
+        }
+    `}
 `;
--- stories/toggle.stories.js
+++ stories/toggle.stories.js
@@ -6,26 +6,45 @@ import Toggle from '../src/components/toggle';
 storiesOf('Toggle', module)
     .add('Uncontrolled', () => <Toggle />)
     .add('Controlled (default unchecked)', () => {
-        const [parentChecked, setParentChecked] = useState(false);
-        return (
-            <div>
-                <Toggle onChange={() => setParentChecked(!parentChecked)} />
-                <span>Checked: {parentChecked ? 'true' : 'false'}</span>
-            </div>
-        );
+        function Controlled() {
+            const [parentChecked, setParentChecked] = useState(false);
+            return (
+                <div>
+                    <Toggle onChange={() => setParentChecked(!parentChecked)} />
+                    <span>Checked: {parentChecked ? 'true' : 'false'}</span>
+                </div>
+            );
+        }
+        return <Controlled />;
     })
     .add('Controlled (default checked)', () => {
-        const [parentChecked, setParentChecked] = useState(true);
-        return (
-            <div>
-                <Toggle checked={parentChecked} onChange={() => setParentChecked(!parentChecked)} />
-                <span>Checked: {parentChecked ? 'true' : 'false'}</span>
-            </div>
-        );
+        function Controlled() {
+            const [parentChecked, setParentChecked] = useState(true);
+            return (
+                <div>
+                    <Toggle
+                        checked={parentChecked}
+                        onChange={() => setParentChecked(!parentChecked)}
+                    />
+                    <span>Checked: {parentChecked ? 'true' : 'false'}</span>
+                </div>
+            );
+        }
+        return <Controlled />;
     })
     .add('Disabled', () => (
         <>
             <Toggle checked disabled />
             <Toggle disabled />
         </>
+    ))
+    .add('With label', () => (
+        <div style={{ alignItems: 'center', display: 'flex' }}>
+            <Toggle id="storybook-toggle" />
+            <label htmlFor="storybook-toggle">Toggle me</label>
+        </div>
+    ))
+    .add('Custom attributes', () => <Toggle color="magenta" height="40px" />)
+    .add('Arbitrary props', () => (
+        <Toggle aria-label="An arbitrary label" style={{ transform: 'rotate(45deg)' }} />
     ));
