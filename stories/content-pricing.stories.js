ecec776a44d463cfc30fc83846711773a1e63c72
Daniel Sato <dksato@gmail.com>
2020-06-29 12:50:02 -0500

    Add additional permutations to content pricing

--- stories/content-pricing.stories.js
+++ stories/content-pricing.stories.js
@@ -13,7 +13,38 @@ storiesOf('Content Pricing', module)
     .add('Missing offer price', () => <Pricing {...{ listPrice }} />)
     .add('Free content', () => <Pricing {...{ offerPrice: '0', listPrice }} />)
     .add('Free content, missing list price', () => <Pricing {...{ offerPrice: '0' }} />)
-    .add('Content with a payment waiver', () => (
-        <Pricing {...{ offerPrice: 'waived', listPrice }} />
+    .add('Text as offer price', () => <Pricing {...{ offerPrice: 'Bork bork bork', listPrice }} />)
+    .add('With waiver', () => <Pricing {...{ offerPrice, listPrice, isWaived: true }} />)
+    .add('With child', () => (
+        <Pricing {...{ offerPrice, listPrice }}>
+            <span
+                style={{
+                    background: 'purple',
+                    margin: '0 0 0 2px',
+                    padding: '2px 5px',
+                    color: 'white',
+                    borderRadius: '10px',
+                    fontSize: '10px',
+                }}
+            >
+                Child Element
+            </span>
+        </Pricing>
+    ))
+    .add('With waiver and child', () => (
+        <Pricing {...{ offerPrice, listPrice, isWaived: true }}>
+            <span
+                style={{
+                    background: 'purple',
+                    margin: '0 0 0 2px',
+                    padding: '2px 5px',
+                    color: 'white',
+                    borderRadius: '10px',
+                    fontSize: '10px',
+                }}
+            >
+                Child Element
+            </span>
+        </Pricing>
     ))
     .add('Missing both prices (why do we allow this?)', () => <Pricing />);
